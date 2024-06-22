413d324f9c3d01695d2983aef954536630d604cb
kenji0512 <kenji.joni@gmail.com>
2024-06-23 03:58:22 +0900

    Playerやスコア、アイテムをゲットし時に使えるスクリプト作成

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+using Dreamteck.Forever;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Runner _Player;
+    public float _moveSpeed = 5f;
+    public float _clampWidth = 4f;
+
+    void Start()
+    {
+        _Player = GetComponent<Runner>();
+    }
+
+    private void Update()
+    {
+        _Player.motion.offset += new Vector2(Input.GetAxis("Horizontal"), 0f) * _moveSpeed * Time.deltaTime;
+        _Player.motion.offset = new Vector2(Mathf.Clamp(_Player.motion.offset.x, -_clampWidth, _clampWidth), _Player.motion.offset.y);
+
+
+        if (transform.position.x < -4) transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+        if (transform.position.x > 4) transform.position = new Vector3(4, transform.position.y, transform.position.z);
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        Runner runner = _Player;
+        runner.follow = false;
+    }
+}
